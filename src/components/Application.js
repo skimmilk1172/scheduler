c8e8fe1c95ecc80c61d845eb22ef828ed0f2b01c
skimmilk1172 <sean.sihun.kim@gmail.com>
2020-10-28 20:42:04 -0400

    useEffect and axios in Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 import DayList from 'components/DayList';
@@ -67,35 +68,24 @@ const appointments = [
 
 
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
-
 export default function Application(props) {
 
+  
   const appointmentList = appointments.map(appointment => (
     <Appointment key={appointment.id} {...appointment}>
       <h4 className="interviewers__header text--light">interviewer</h4>
       <ul className="interviewers__list"></ul>
     </Appointment>
   ));
-
+  
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    axios.get(`/api/days`).then(response => {
+      setDays(() => response.data);
+    });
+  }, [day]);
 
   console.log(day);
 
